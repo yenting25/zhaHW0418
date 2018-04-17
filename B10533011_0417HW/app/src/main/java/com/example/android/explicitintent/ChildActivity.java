a38d806f1572e5cb3e313d1d6afdf9314c8f9758
yenting25 <37055295+yenting25@users.noreply.github.com>
2018-04-17 22:41:41 +0800

    0418

---
+++ B10533011_0417HW/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.explicitintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ChildActivity extends AppCompatActivity {
+
+    private TextView mDisplayText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        mDisplayText=(TextView)findViewById(R.id.tv_display);
+        Intent intentThatStartedThisActivity = getIntent();
+        if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+            String textEntered = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+            mDisplayText.setText(textEntered);
+        }
+    }
+}
